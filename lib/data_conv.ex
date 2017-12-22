57bb590e104f3022c873c6a59044aaa049db64e1
Declan Whelan <dwhelan@dpwhelan.com>
2017-12-21 21:57:33 -0500

    Initial commit

---
+++ lib/data_conv.ex
@@ -0,0 +1,18 @@
+defmodule DataConv do
+  @moduledoc """
+  Documentation for DataConv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DataConv.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/data_conv_test.exs
@@ -0,0 +1,8 @@
+defmodule DataConvTest do
+  use ExUnit.Case
+  doctest DataConv
+
+  test "greets the world" do
+    assert DataConv.hello() == :world
+  end
+end
