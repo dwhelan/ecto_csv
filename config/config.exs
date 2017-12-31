4627371fbebc7c70248e1c7cf74b7cc76cd7ef41
Declan Whelan <dwhelan@dpwhelan.com>
2017-12-30 20:09:25 -0500

    Added environment config files

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,9 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :logger,
+       compile_time_purge_level: :info
+        
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
@@ -27,4 +30,4 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-#     import_config "#{Mix.env}.exs"
+import_config "#{Mix.env}.exs"
