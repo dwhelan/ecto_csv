c67cb6bfabf6c4898b6a336d0c0d059ba635629c
Declan Whelan <dwhelan@dpwhelan.com>
2017-12-30 23:28:17 -0500

    Ignore unused options in Column.input/2.

--- lib/csv/column.ex
+++ lib/csv/column.ex
@@ -1,10 +1,20 @@
 defmodule CSV.Column do
 
-  def input({_, transform: transform}, value) do
-    {:ok, [transform.(value), value]}
-  end
+#  def input(col = {_, type: Integer}, value) do
+#    input(col, Keyword.delete())
+#    {:ok, [transform.(value), value]}
+#  end
 
-  def input(_, value) do
-    {:ok, [value]}
+  def input({name , options}, value) do
+    transform = options[:transform]
+    if transform do
+      input({name, Keyword.delete(options, :transform)}, transform.(value))
+    else
+      {:ok, value}
+    end
   end
+#
+#  def input(_, value) do
+#    {:ok, [value]}
+#  end
 end
--- test/csv/column_test.exs
+++ test/csv/column_test.exs
@@ -3,14 +3,25 @@ defmodule CSV.ColumnTest do
   require CSV.Column, as: Column
 
   describe "input" do
-    test "with no transforms should return a cell with the value" do
+    test "with no transform should return a cell with the value" do
       col  = {"Name", []}
-      assert Column.input(col, "foo") === {:ok, ["foo"]}
+      assert Column.input(col, "foo") === {:ok, "foo"}
     end
 
-    test "with a transfor should return a cell with the transformed value" do
+#    test "with a type should return a cell with the parsed value" do
+#      col  = {"Name", [type: Integer]}
+#      assert Column.input(col, "123") === {:ok, [123]}
+#    end
+
+    test "with a transform should return a cell with the transformed value" do
       col  = {"Name", [transform: &String.upcase/1]}
-      assert Column.input(col, "foo") === {:ok, ["FOO", "foo"]}
+      assert Column.input(col, "foo") === {:ok, "FOO"}
     end
+
+    test "should ignore extra options" do
+      col  = {"Name", [transform: &String.upcase/1, foo: "bar"]}
+      assert Column.input(col, "foo") === {:ok, "FOO"}
+    end
+
   end
 end
