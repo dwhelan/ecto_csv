36634295d2c79c30dc10d669f66ddbe13d2ae40a
Declan Whelan <dwhelan@dpwhelan.com>
2017-12-31 00:46:16 -0500

    Refactored Column.input to a flow execution

--- lib/csv/column.ex
+++ lib/csv/column.ex
@@ -1,20 +1,46 @@
 defmodule CSV.Column do
+  def input({_name, options}, value) do
+    {:ok, value, options}
+    |> type
+    |> transform
+    |> result
+  end
 
-#  def input(col = {_, type: Integer}, value) do
-#    input(col, Keyword.delete())
-#    {:ok, [transform.(value), value]}
-#  end
-
-  def input({name , options}, value) do
-    transform = options[:transform]
-    if transform do
-      input({name, Keyword.delete(options, :transform)}, transform.(value))
+  defp type({:ok, value, options}) do
+    if type = options[:type] do
+      {:ok, parse(type, value), delete(options, :type)}
     else
-      {:ok, value}
+      {:ok, value, options}
     end
   end
-#
-#  def input(_, value) do
-#    {:ok, [value]}
-#  end
+
+  defp transform({:ok, value, options}) do
+    if transform = options[:transform] do
+      {:ok, transform(transform, value), delete_first(options, :transform)}
+    else
+      {:ok, value, options}
+    end
+  end
+
+  defp result{status, value, _options} do
+    {status, value}
+  end
+
+  defp delete(options, key) do
+    Keyword.delete(options, key)
+  end
+
+  defp delete_first(options, key) do
+    Keyword.delete_first(options, key)
+  end
+
+  defp parse(Integer, value),
+    do: String.to_integer(value)
+
+  defp parse(String, value),
+    do: value
+
+  defp transform(f, value) do
+    f.(value)
+  end
 end
--- test/csv/column_test.exs
+++ test/csv/column_test.exs
@@ -2,16 +2,23 @@ defmodule CSV.ColumnTest do
   use ExUnit.Case
   require CSV.Column, as: Column
 
+  describe "input type" do
+    test "Integer" do
+      col  = {"Name", [type: Integer]}
+      assert Column.input(col, "123") === {:ok, 123}
+    end
+
+    test "String" do
+      col  = {"Name", [type: String]}
+      assert Column.input(col, "123") === {:ok, "123"}
+    end
+  end
+
   describe "input" do
     test "with no transform should return a cell with the value" do
       col  = {"Name", []}
       assert Column.input(col, "foo") === {:ok, "foo"}
     end
-
-#    test "with a type should return a cell with the parsed value" do
-#      col  = {"Name", [type: Integer]}
-#      assert Column.input(col, "123") === {:ok, [123]}
-#    end
 
     test "with a transform should return a cell with the transformed value" do
       col  = {"Name", [transform: &String.upcase/1]}
