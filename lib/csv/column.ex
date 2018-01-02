c46ba721c16cc1c394190b25ac6cf999da091b5b
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-01 23:10:00 -0500

    Minoe renames for clarity.

--- lib/csv/column.ex
+++ lib/csv/column.ex
@@ -3,14 +3,14 @@ defmodule CSV.Column do
   require CSV.Invoke, as: Invoke
 
   def transform(value, transforms, options \\ %{}) do
-    Enum.reduce(List.wrap(transforms), {:ok, value}, fn(transform, value) -> apply_transform(transform, value, options) end)
+    Enum.reduce(List.wrap(transforms), {:ok, value}, fn(transform, value) -> transform_one(transform, value, options) end)
   end
 
-  defp apply_transform(_, {:error, errors}, _) do
+  defp transform_one(_, {:error, errors}, _) do
     {:error, errors}
   end
 
-  defp apply_transform(transform, {:ok, value}, options) do
+  defp transform_one(transform, {:ok, value}, options) do
     try do
       {:ok, Invoke.call(transform, value, options[:modules] || []) }
     rescue
--- test/csv/column_test.exs
+++ test/csv/column_test.exs
@@ -2,81 +2,76 @@ defmodule CSV.ColumnTest do
   use ExUnit.Case
   require CSV.Column, as: Column
 
-  def integer(string) do
+  def to_int(string) do
     String.to_integer(string)
   end
 
   describe "transform function" do
     test "as a String with module" do
-      transform = {CSV.ColumnTest, "integer"}
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = {CSV.ColumnTest, "to_int"}
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as an Atom with module" do
-      transform = {CSV.ColumnTest, :integer}
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = {CSV.ColumnTest, :to_int}
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as a String that includes module name" do
-      transform = "CSV.ColumnTest.integer"
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = "CSV.ColumnTest.to_int"
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as an Atom that includes module name" do
-      transform = :"CSV.ColumnTest.integer"
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = :"CSV.ColumnTest.to_int"
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as a lambda to a named function" do
-      transform = &integer/1
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = &to_int/1
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as a lambda to an anonymous function" do
-      transform = &String.to_integer(&1)
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = &(String.to_integer(&1))
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
   end
 
   describe "transform module" do
     test "as a Module" do
-      transform = {CSV.ColumnTest, :integer}
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = {CSV.ColumnTest, :to_int}
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as a String" do
-      transform = {"CSV.ColumnTest", :integer}
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = {"CSV.ColumnTest", :to_int}
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as an Atom" do
-      transform = {:"CSV.ColumnTest", :integer}
-      assert Column.transform("123", transform) === {:ok, 123}
+      to_int = {:"CSV.ColumnTest", :to_int}
+      assert Column.transform("123", to_int) === {:ok, 123}
     end
 
     test "as the first module in options[:modules]" do
-      transform = :integer
-      assert Column.transform("123", transform, modules: [CSV.ColumnTest, String]) === {:ok, 123}
+      assert Column.transform("123", :to_int, modules: [CSV.ColumnTest, String]) === {:ok, 123}
     end
 
     test "as the last module in options[:modules]" do
-      transform = :integer
-      assert Column.transform("123", transform, modules: [String, CSV.ColumnTest]) === {:ok, 123}
+      assert Column.transform("123", :to_int, modules: [String, CSV.ColumnTest]) === {:ok, 123}
     end
 
     test "as the only module in options[:modules]" do
-      transform = :integer
-      assert Column.transform("123", transform, modules: [CSV.ColumnTest]) === {:ok, 123}
+      assert Column.transform("123", :to_int, modules: [CSV.ColumnTest]) === {:ok, 123}
     end
 
     test "as the options[:modules]" do
-      transform = :integer
-      assert Column.transform("123", transform, modules: CSV.ColumnTest) === {:ok, 123}
+      assert Column.transform("123", :to_int, modules: CSV.ColumnTest) === {:ok, 123}
     end
   end
 
   test "should ignore extra options" do
-    transform = &integer/1
-    assert Column.transform("123", transform, foo: "bar") === {:ok, 123}
+    assert Column.transform("123", &to_int/1, foo: "bar") === {:ok, 123}
   end
 end
