62eeafbf5b6b4a003225d678d3a0205a9c4eca90
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-01 21:38:57 -0500

    Removed dead code

--- lib/csv/column.ex
+++ lib/csv/column.ex
@@ -42,99 +42,24 @@ defmodule CSV.Column do
     end
   end
 
-  def input2({_name, transforms}, value, options \\ %{}) do
-    transform2(value, List.wrap(transforms), options)
+  def input({_name, transforms}, value, options \\ %{}) do
+    transform(value, List.wrap(transforms), options)
   end
 
-  defp transform2(value, transforms, options) do
-    Enum.reduce(transforms, {:ok, value}, fn(transform, value) -> apply_transform2(transform, value, options) end)
+  defp transform(value, transforms, options) do
+    Enum.reduce(transforms, {:ok, value}, fn(transform, value) -> apply_transform(transform, value, options) end)
   end
 
-  defp apply_transform2(_transform, {:error, errors}, _options) do
+  defp apply_transform(_transform, {:error, errors}, _options) do
     {:error, errors}
   end
 
-  defp apply_transform2(transform, {:ok, value}, options) do
+  defp apply_transform(transform, {:ok, value}, options) do
     try do
       {:ok, Invoke.call(transform, value, options[:modules] || []) }
     rescue
-      # e in FunctionClauseError    -> {:error, [FunctionClauseError.message(e)]}
+      e in FunctionClauseError    -> {:error, [FunctionClauseError.message(e)]}
       e in UndefinedFunctionError -> {:error, [UndefinedFunctionError.message(e)]}
     end
-  end
-
-  def input({_name, options}, value) do
-    {:ok, options, value}
-    |> parse
-    |> transform
-    |> result
-  end
-
-  defp parse({:ok, options, string}) do
-    if type = options[:type] do
-      {status, result } =  apply_parse(type, string)
-      {status, delete(options, :type), result}
-    else
-      {:ok, options, string}
-    end
-  end
-
-  defp transform({:ok, options, value}) do
-    if f = options[:transform] do
-      apply_transform(f, delete_first(options, :transform), value) |> transform
-    else
-      {:ok, options, value}
-    end
-  end
-
-  defp transform({:error, options, errors}) do
-    {:error, options, errors}
-  end
-
-  defp result{status, _options, value} do
-    {status, value}
-  end
-
-  defp apply_parse(type, string) do
-    cond do
-      is_a?(String,  type) -> {:ok,    string}
-      is_a?(Integer, type) -> parse_integer(string)
-      true                 -> {:error, ["unknown type '#{type}'"]}
-    end
-  end
-
-  # @parsers %{Integer => &CSV.Column.parse_integer/1}
-
-  defp parse_integer(string) do
-    case Integer.parse(string) do
-      {integer, _} -> {:ok,    integer}
-      :error       -> {:error, ["'#{string}' is not an Integer"]}
-    end
-  end
-
-  defp is_a?(module, type) do
-    "#{module}" in ["#{type}", "Elixir.#{type}"]
-  end
-
-  defp apply_transform(f, options, value) do
-    try do
-      if is_function(f) do
-        {:ok, options, f.(value)}
-      else
-        f_atom = if is_atom(f), do: f, else: String.to_atom(f)
-        {:ok, options, Kernel.apply(String, f_atom, [value])}
-      end
-    rescue
-      e in FunctionClauseError    -> {:error, options, [FunctionClauseError.message(e)]}
-      e in UndefinedFunctionError -> {:error, options, [UndefinedFunctionError.message(e)]}
-    end
-  end
-
-  defp delete(options, key) do
-    Keyword.delete(options, key)
-  end
-
-  defp delete_first(options, key) do
-    Keyword.delete_first(options, key)
   end
 end
--- test/csv/column_test.exs
+++ test/csv/column_test.exs
@@ -9,154 +9,75 @@ defmodule CSV.ColumnTest do
   describe "transform function" do
     test "as a String" do
       col = {"Name", {CSV.ColumnTest, "integer"} }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as an Atom" do
       col = {"Name", {CSV.ColumnTest, :integer} }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as a String that includes module name" do
       col = {"Name", "CSV.ColumnTest.integer" }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as an Atom that includes module name" do
       col = {"Name", :"CSV.ColumnTest.integer" }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as a lambda to a named function" do
       col = {"Name", &integer/1}
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as a lambda to an anonymous function" do
       col = {"Name", &String.to_integer(&1)}
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
   end
 
-  describe "transform modules" do
+  describe "transform module" do
     test "as a Module" do
       col = {"Name", {CSV.ColumnTest, :integer} }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as a String" do
       col = {"Name", {"CSV.ColumnTest", :integer} }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as an Atom" do
       col = {"Name", {:"CSV.ColumnTest", :integer} }
-      assert Column.input2(col, "123") === {:ok, 123}
+      assert Column.input(col, "123") === {:ok, 123}
     end
 
     test "as the first module in options[:modules]" do
-      col = {"Name", :integer }
-      assert Column.input2(col, "123", modules: [CSV.ColumnTest, String]) === {:ok, 123}
+      col = {"Name", :integer}
+      assert Column.input(col, "123", modules: [CSV.ColumnTest, String]) === {:ok, 123}
     end
 
     test "as the last module in options[:modules]" do
-      col = {"Name", :integer }
-      assert Column.input2(col, "123", modules: [String, CSV.ColumnTest]) === {:ok, 123}
+      col = {"Name", :integer}
+      assert Column.input(col, "123", modules: [String, CSV.ColumnTest]) === {:ok, 123}
     end
 
     test "as the only module in options[:modules]" do
-      col = {"Name", :integer }
-      assert Column.input2(col, "123", modules: [CSV.ColumnTest]) === {:ok, 123}
+      col = {"Name", :integer}
+      assert Column.input(col, "123", modules: [CSV.ColumnTest]) === {:ok, 123}
     end
 
     test "as the options[:modules]" do
-      col = {"Name", :integer }
-      assert Column.input2(col, "123", modules: CSV.ColumnTest) === {:ok, 123}
-    end
-  end
-
-  describe "input type" do
-    test "Integer" do
-      col = {"Name", [type: Integer]}
-      assert Column.input(col, "123") === {:ok, 123}
-    end
-
-    test "Integer atom" do
-      col = {"Name", [type: :Integer]}
-      assert Column.input(col, "123") === {:ok, 123}
-    end
-
-    test "Integer string" do
-      col = {"Name", [type: "Integer"]}
-      assert Column.input(col, "123") === {:ok, 123}
-    end
-
-    test "bad Integer" do
-      col = {"Name", [type: Integer]}
-      assert Column.input(col, "abc") === {:error, ["'abc' is not an Integer"]}
-    end
-
-    test "String" do
-      col = {"Name", [type: String]}
-      assert Column.input(col, "123") === {:ok, "123"}
-    end
-
-    test "String string" do
-      col = {"Name", [type: "String"]}
-      assert Column.input(col, "123") === {:ok, "123"}
-    end
-
-    test "invalid type" do
-      col = {"Name", [type: "Bad Type"]}
-      assert Column.input(col, "123") === {:error, ["unknown type 'Bad Type'"]}
-    end
-
-    test "should use first type with multiple types" do
-      col = {"Name", [type: String, type: Integer]}
-      assert Column.input(col, "123") === {:ok, "123"}
-    end
-  end
-
-  describe "input transform" do
-    test "with no transform should return a cell with the value" do
-      col = {"Name", []}
-      assert Column.input(col, "foo") === {:ok, "foo"}
-    end
-
-    test "should return transformed value" do
-      col = {"Name", [transform: &String.upcase/1]}
-      assert Column.input(col, "foo") === {:ok, "FOO"}
-    end
-
-    test "should allow String functions to be specified with an Atom" do
-      col = {"Name", [transform: :upcase]}
-      assert Column.input(col, "foo") === {:ok, "FOO"}
-    end
-
-    test "should allow String functions to be specified with an String" do
-      col = {"Name", [transform: "upcase"]}
-      assert Column.input(col, "foo") === {:ok, "FOO"}
-    end
-
-    test "should multiple transforms" do
-      col = {"Name", [transform: "upcase", transform: "capitalize"]}
-      assert Column.input(col, "foo") === {:ok, "Foo"}
-    end
-
-    test "with an invalid function should return the error message" do
-      col = {"Name", [transform: &Foo.bar/1]}
-      assert Column.input(col, "foo") === {:error, ["function Foo.bar/1 is undefined (module Foo is not available)"]}
-    end
-
-    test "with a pattern matching error should return the error message" do
-      col = {"Name", [transform: &Integer.digits/1]}
-      assert Column.input(col, "foo") === {:error, ["no function clause matching in Integer.digits/2"]}
+      col = {"Name", :integer}
+      assert Column.input(col, "123", modules: CSV.ColumnTest) === {:ok, 123}
     end
   end
 
   test "should ignore extra options" do
-    col = {"Name", [transform: &String.upcase/1, foo: "bar"]}
-    assert Column.input(col, "foo") === {:ok, "FOO"}
+    col = {"Name", &integer/1}
+    assert Column.input(col, "123", foo: "bar") === {:ok, 123}
   end
 end
 
