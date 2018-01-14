3313f5d54e9a3ea857a809f9bab3629abfcb5bb9
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-14 18:47:34 -0500

    Simplified CSV.Loader

--- lib/csv/loader.ex
+++ lib/csv/loader.ex
@@ -6,28 +6,42 @@ defmodule CSV.Loader do
   end
 
   def load(stream, schema) do
-    stream = stream |> parse
-    
-    headers = hd(Enum.take(stream, 1))
+    headers = headers(stream)
 
     stream
-    |> Stream.with_index
-    |> Stream.filter(fn {_, index} -> index >= 1 end)
-    |> Stream.map(fn {line, _} -> load_row(line, schema, headers) end)
+    |> remove_header
+    |> to_values
+    |> to_struct(schema, headers)
   end
 
-  defp load_row(row, schema, headers) do
-    Enum.zip(headers, row)
-    |> Enum.reduce(struct(schema), fn(name_string, record) -> update(name_string, record, schema) end)
+  defp headers(stream) do
+    hd(Enum.take(stream |> to_values, 1))
   end
 
-  defp update({name, string}, record, schema) do
-    field = String.to_atom(name)
-    value =  CSV.Schema.cast(schema, field, string)
-    struct(record, Keyword.new([{field, value}]))
+  defp remove_header(stream) do
+    Stream.transform(stream, true, fn struct, is_header -> 
+      case is_header do
+        true  -> { [], false }
+        false -> { [struct], false }
+      end
+    end )
   end
 
-  def parse(stream) do
+  def to_values(stream) do
     Parser.parse_stream(stream, headers: false)
+  end
+
+  defp to_struct(stream, schema, headers) do
+    Stream.map(stream, &create_struct(&1, schema, headers))
+  end
+
+  defp create_struct(values, schema, headers) do
+    Enum.zip(headers, values) |> Enum.reduce(struct(schema), &set_struct_value(&1, &2, schema))
+  end
+
+  defp set_struct_value({name, value}, struct, schema) do
+    field = String.to_atom(name)
+    value = CSV.Schema.cast(schema, field, value)
+    struct(struct, Keyword.new([{field, value}]))
   end
 end
