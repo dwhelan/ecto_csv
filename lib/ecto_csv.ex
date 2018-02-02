27f0b239ff2fc9c5a9f3c948bbe441d0556a0d1f
Declan Whelan <dwhelan@dpwhelan.com>
2018-02-01 22:01:08 -0300

    Removed EctoCSV.dump()

--- lib/ecto_csv.ex
+++ lib/ecto_csv.ex
@@ -1,12 +1,2 @@
 defmodule EctoCSV do
-  alias Ecto.Type
-
-  
-  def dump(schema, field, value) do
-    type(schema, field) |> Type.dump(value)
-  end
-  
-  defp type(schema, field) do
-    schema.__schema__(:type, field) || :string
-  end
 end
