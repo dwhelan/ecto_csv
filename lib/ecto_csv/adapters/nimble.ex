782e71af15865616ec2cd8fc4b02ef21fa289def
Declan Whelan <dwhelan@dpwhelan.com>
2018-02-05 15:38:27 -0300

    Created nimble csv engines for comm, tab and pipe separators

--- lib/ecto_csv/adapters/nimble.ex
+++ lib/ecto_csv/adapters/nimble.ex
@@ -1,21 +1,32 @@
 defmodule EctoCSV.Adapters.Nimble do
-  require NimbleCSV.RFC4180
-  NimbleCSV.define(EctoCSV.Adapters.Nimble.Custom, separator: ",", escape: "\"")
-  alias EctoCSV.Adapters.Nimble.Custom
+  NimbleCSV.define(EctoCSV.Adapters.Nimble.CommaSeparator, separator: ",",  escape: "\"")
+  NimbleCSV.define(EctoCSV.Adapters.Nimble.PipeSeparator,  separator: "|",  escape: "\"")
+  NimbleCSV.define(EctoCSV.Adapters.Nimble.TabSeparator,   separator: "\t", escape: "\"")
+
+  alias EctoCSV.Adapters.Nimble.CommaSeparator 
+  alias EctoCSV.Adapters.Nimble.PipeSeparator
+  alias EctoCSV.Adapters.Nimble.TabSeparator
 
   def read(stream, schema) do
-    create_custom_nimble(schema).parse_stream(stream, headers: false)
+    nimble_for(schema).parse_stream(stream, headers: false)
   end
 
   def write(stream, schema) do
-    create_custom_nimble(schema).dump_to_stream(stream)
+    nimble_for(schema).dump_to_stream(stream)
   end
 
-  defp create_custom_nimble(schema) do
-    Code.compiler_options(ignore_module_conflict: true)
-    NimbleCSV.define(Custom, options(schema))
-    Code.compiler_options(ignore_module_conflict: false)
-    Custom
+  defp nimble_for(schema) do
+    case options(schema)[:separator] do
+      ","  -> CommaSeparator
+      "|"  -> PipeSeparator
+      "\t" -> TabSeparator
+      _   -> custom_for(schema)
+    end
+  end
+
+  defp custom_for(schema) do
+    NimbleCSV.define(EctoCSV.Adapters.Nimble.Custom, options(schema))
+    EctoCSV.Adapters.Nimble.Custom
   end
 
   defp options(schema) do
